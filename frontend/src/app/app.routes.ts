9ccd04f681b02412013ddab35eedb5b66f1094fc
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2025-03-18 17:10:57 +0100

    feat(frontend): add routes for home, login and registration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './layout/home/home.component';
+import { LoginComponent } from './features/login/login.component';
+import { RegistrationComponent } from './features/registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', 
+        component: HomeComponent,
+        title: 'MyJobs - home' 
+    },
+    { 
+        path: 'login',
+        // canActivate: [UnauthGuard], 
+        component: LoginComponent,
+        title: 'Login',
+    },
+    { 
+        path: 'register',
+        // canActivate: [UnauthGuard],
+        component: RegistrationComponent, 
+        title: 'Registration', 
+    },
+];
