228e2d8dfb3e4cb8d6c1107a7169a822f074e7bc
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2025-04-04 16:32:51 +0200

    test: added some tests for job metadata extraction

--- backend/core/src/test/java/com/wilzwert/myjobs/core/domain/ports/driven/metadata/extractor/JsonLdJobMetadataExtractorTest.java
+++ backend/core/src/test/java/com/wilzwert/myjobs/core/domain/ports/driven/metadata/extractor/JsonLdJobMetadataExtractorTest.java
@@ -1,8 +1,8 @@
 package com.wilzwert.myjobs.core.domain.ports.driven.metadata.extractor;
 
 
-import com.wilzwert.myjobs.core.domain.service.metadata.extractor.ExtractedMetadata;
-import com.wilzwert.myjobs.core.domain.service.metadata.extractor.JsonLdJobMetadataExtractor;
+import com.wilzwert.myjobs.core.domain.model.JobMetadata;
+import com.wilzwert.myjobs.core.domain.ports.driven.metadata.extractor.impl.JsonLdJobMetadataExtractor;
 import org.junit.jupiter.api.Test;
 import utils.TestFileLoader;
 import java.io.IOException;
@@ -20,30 +20,30 @@ public class JsonLdJobMetadataExtractorTest {
 
     @Test
     public void testEmptyJsonExtraction() {
-        assertTrue(extractor.getMetadata("").isEmpty());
+        assertTrue(extractor.extractJobMetadata("").isEmpty());
     }
 
     @Test
     public void testIncorrectJsonExtraction() {
-        assertTrue(extractor.getMetadata("this is not json").isEmpty());
+        assertTrue(extractor.extractJobMetadata("this is not json").isEmpty());
     }
 
     @Test
     public void testNotJsonLdExtraction() {
-        assertTrue(extractor.getMetadata("{\"field\":\"value\"}").isEmpty());
+        assertTrue(extractor.extractJobMetadata("{\"field\":\"value\"}").isEmpty());
     }
 
     @Test
     public void testNotJobPostingExtraction() throws IOException {
         String html  = TestFileLoader.loadFileAsString("product.jsonld.html");
-        assertTrue(extractor.getMetadata(html ).isEmpty());
+        assertTrue(extractor.extractJobMetadata(html ).isEmpty());
     }
 
     @Test
     public void testWithNoSalaryExtraction() throws IOException {
         String html  = TestFileLoader.loadFileAsString("jobposting.jsonld.nosalary.html");
 
-        ExtractedMetadata expectedMetadata = new ExtractedMetadata.Builder()
+        JobMetadata expectedMetadata = new JobMetadata.Builder()
                 .title("Stage - chargé de communication/événementiel H/F")
                 .description("Afin de poursuivre son développement, L4M recrute un(e) communicant(e) ayant le goût pour l'événementiel, de bonnes capacités rédactionnelles et à l'aise avec les outils informatiques et le téléphone. Vous aurez pour principales missions : - le...")
                 .profile("Avant toute chose, un réel intérêt pour les missions et l'entreprise :   - Niveau Bac +3 acquis ou en cours,   - Bon niveau rédactionnel,   - Curieux, enthousiaste et autonome,   - Réactif, polyvalent, bonne gestion du stress,   - Connaissance des rése...")
@@ -52,7 +52,7 @@ public class JsonLdJobMetadataExtractorTest {
                 .salary("")
                 .build();
 
-        extractor.getMetadata(html).ifPresentOrElse(
+        extractor.extractJobMetadata(html).ifPresentOrElse(
             extractedMetadata -> assertEquals(expectedMetadata, extractedMetadata),
             () -> fail("Metadata should not be empty")
         );
@@ -62,7 +62,7 @@ public class JsonLdJobMetadataExtractorTest {
     public void testWithSalaryMonetaryAmountExtraction() throws IOException {
         String html  = TestFileLoader.loadFileAsString("jobposting.jsonld.salary.monetaryamount.html");
 
-        ExtractedMetadata expectedMetadata = new ExtractedMetadata.Builder()
+        JobMetadata expectedMetadata = new JobMetadata.Builder()
                 .title("Formateur equipes commerciales h/f")
                 .description("<p>Quelles sont les missions ?</p><p>&nbsp;Rejoignez Le Service Formation !<br/>Votre r&ocirc;le&nbsp;<br/>Dans le cadre de notre d&eacute;veloppement, nous recherchons&nbsp;un Formateur Interne au niveau national&nbsp;d&eacute;di&eacute; Commerce,&nbsp;pour renforcer notre &eacute;quipe de formateurs. V&eacute;ritable ambassadeur de la performance commerciale et de l'excellence, vous jouerez un r&ocirc;le cl&eacute; dans le d&eacute;veloppement des comp&eacute;tences de nos &eacute;quipes commerciales.<br/>Vous accompagnerez nos collaborateurs commerciaux vers le succ&egrave;s en assurant leur formation + pilotage Sales Academy.<br/>Formation aux techniques de vente :<br/>\t-\tConcevoir, animer et &eacute;valuer des formations&nbsp;aux techniques de vente et sur nos produits et services<br/>\t-\tAssurer un suivi r&eacute;gulier des performances des collaborateurs form&eacute;s<br/>\t-\tAccompagnement terrain des commerciaux<br/>\t-\tIdentifier les besoins de formation et optimiser les processus d'apprentissage en collaboration avec avec la direction commerciale<br/></p><p>Quel est le profil idéal ?</p><p>Ing&eacute;nierie p&eacute;dagogique&nbsp;:<br/>\t-\tAdapter les contenus p&eacute;dagogiques en fonction des besoins des &eacute;quipes et des &eacute;volutions du march&eacute;.<br/>Veille et innovation :<br/>\t-\tProposer des outils et formats innovants pour favoriser l'apprentissage (e-learning, ateliers pratiques, &eacute;tudes de cas, etc.).<br/>\t-\tAssurer une veille sur les meilleures pratiques commerciales et les nouvelles m&eacute;thodes de formation<br/>Intervenant dans la Sales Academy et int&eacute;gration des nouveaux commerciaux :<br/>\t-\tStructurer et coordonner la Sales Academy, garantissant un parcours de formation coh&eacute;rent et efficace pour les nouveaux arrivants<br/>\t-\tOrganiser et superviser l'int&eacute;gration des nouveaux commerciaux pour assurer une mont&eacute;e en comp&eacute;tence rapide<br/>Ce poste est fait pour vous si ....<br/>Vous justifiez d'une exp&eacute;rience r&eacute;ussie dans le commerce dans la vente ou le management commercial et d'une exp&eacute;rience en formation<br/>\t-\tComp&eacute;tences p&eacute;dagogiques : capacit&eacute; &agrave; transmettre vos connaissances et &agrave; engager<br/>\t-\tOrganisation et leadership : piloter des projets, structurer des parcours de formation et mobiliser des &eacute;quipes.<br/>\t-\tD&eacute;placements &agrave; pr&eacute;voir dans toute la France 3j/semaine<br/>Pourquoi nous rejoindre ?<br/>\t-\tUn groupe International en croissance depuis plus de 10 ans<br/>\t-\tUn parcours d'int&eacute;gration et formation aux produits et services<br/>\t-\tUn management de proximit&eacute;, des &eacute;quipes dynamiques<br/>\t-\tOpportunit&eacute;s de d&eacute;veloppement professionnel et carri&egrave;re<br/>Avantages<br/>\t-\tMutuelle / Pr&eacute;voyance / CE / Compte Epargne Temps / RTT&nbsp;<br/>\t-\tVoiture de fonction&nbsp;<br/>R&eacute;mun&eacute;ration&nbsp;:&nbsp; &agrave; partir de 50K&curren; +10% de variable&nbsp;<br/>Statut cadre<br/>Visitez notre site :&nbsp;&nbsp;&nbsp;https://careers.rentokil-initial.com/fr/<br/>Rentosj<br/></p><p>Qui a publié cette offre ?</p><p>D&eacute;couvrez un univers professionnel &eacute;tonnant !<br/><br/>Depuis 1923, Initial Textile, acteur majeur de la blanchisserie industrielle, accompagne les soci&eacute;t&eacute;s qui souhaitent externaliser le nettoyage de leurs v&ecirc;tements et linge professionnels. <br/><br/>Plus de 50 000 clients de l'industrie, de l'h&ocirc;tellerie-restauration, artisans et commerces de proximit&eacute; nous font confiance.<br/><br/>Nos 25 sites en France g&egrave;rent la location/entretien du linge et la livraison avec l'ambition d'apporter le meilleur niveau de service.<br/><br/>Seul expert de ce march&eacute; &agrave; r&eacute;aliser la cr&eacute;ation de v&ecirc;tements, nos gammes allient confort, style et s&eacute;curit&eacute;, adapt&eacute;es aux sp&eacute;cificit&eacute;s de chaque m&eacute;tier. La tra&ccedil;abilit&eacute; de nos articles, apporte une valeur ajout&eacute;e unique &agrave; notre prestation de service.<br/><br/>Dans le cadre de notre politique RSE, 100% du linge est recycl&eacute; vers d'autres industries et nous poursuivons nos engagements sur la transition &eacute;nerg&eacute;tique.<br/><br/><br/><br/></p>")
                 .profile("Ingénierie pédagogique :\n\t-\tAdapter les contenus pédagogiques en fonction des besoins des équipes et des évolutions du marché.\nVeille et innovation :\n\t-\tProposer des outils et formats innovants pour favoriser l'apprentissage (e-learning, ateliers pratiques, études de cas, etc.).\n\t-\tAssurer une veille sur les meilleures pratiques commerciales et les nouvelles méthodes de formation\nIntervenant dans la Sales Academy et intégration des nouveaux commerciaux :\n\t-\tStructurer et coordonner la Sales Academy, garantissant un parcours de formation cohérent et efficace pour les nouveaux arrivants\n\t-\tOrganiser et superviser l'intégration des nouveaux commerciaux pour assurer une montée en compétence rapide\nCe poste est fait pour vous si ....\nVous justifiez d'une expérience réussie dans le commerce dans la vente ou le management commercial et d'une expérience en formation\n\t-\tCompétences pédagogiques : capacité à transmettre vos connaissances et à engager\n\t-\tOrganisation et leadership : piloter des projets, structurer des parcours de formation et mobiliser des équipes.\n\t-\tDéplacements à prévoir dans toute la France 3j/semaine\nPourquoi nous rejoindre ?\n\t-\tUn groupe International en croissance depuis plus de 10 ans\n\t-\tUn parcours d'intégration et formation aux produits et services\n\t-\tUn management de proximité, des équipes dynamiques\n\t-\tOpportunités de développement professionnel et carrière\nAvantages\n\t-\tMutuelle / Prévoyance / CE / Compte Epargne Temps / RTT \n\t-\tVoiture de fonction \nRémunération :  à partir de 50K? +10% de variable \nStatut cadre\nVisitez notre site :   https://careers.rentokil-initial.com/fr/\nRentosj\n")
@@ -71,7 +71,7 @@ public class JsonLdJobMetadataExtractorTest {
                 .salary("30000 - 60000 EUR / YEAR")
                 .build();
 
-        extractor.getMetadata(html).ifPresentOrElse(
+        extractor.extractJobMetadata(html).ifPresentOrElse(
                 extractedMetadata -> assertEquals(expectedMetadata, extractedMetadata),
                 () -> fail("Metadata should not be empty")
         );
@@ -81,7 +81,7 @@ public class JsonLdJobMetadataExtractorTest {
     public void testWithSalaryNumberExtraction() throws IOException {
         String html  = TestFileLoader.loadFileAsString("jobposting.jsonld.salary.number.html");
 
-        ExtractedMetadata expectedMetadata = new ExtractedMetadata.Builder()
+        JobMetadata expectedMetadata = new JobMetadata.Builder()
                 .title("Développeur Java")
                 .description("Nous recherchons un développeur Java expérimenté.")
                 .profile("3 ans d'expérience minimum.")
@@ -90,7 +90,7 @@ public class JsonLdJobMetadataExtractorTest {
                 .salary("45000")
                 .build();
 
-        extractor.getMetadata(html).ifPresentOrElse(
+        extractor.extractJobMetadata(html).ifPresentOrElse(
                 extractedMetadata -> assertEquals(expectedMetadata, extractedMetadata),
                 () -> fail("Metadata should not be empty")
         );
---
+++ backend/core/src/test/java/com/wilzwert/myjobs/core/domain/service/metadata/JobMetadataExtractorServiceTest.java
@@ -0,0 +1,110 @@
+package com.wilzwert.myjobs.core.domain.service.metadata;
+
+
+import com.wilzwert.myjobs.core.domain.exception.MalformedUrlException;
+import com.wilzwert.myjobs.core.domain.model.JobMetadata;
+import com.wilzwert.myjobs.core.domain.ports.driven.metadata.extractor.JobMetadataExtractor;
+import com.wilzwert.myjobs.core.domain.ports.driven.metadata.fetcher.HtmlFetcherService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import utils.TestFileLoader;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Wilhelm Zwertvaegher
+ * Date:04/04/2025
+ * Time:15:14
+ */
+
+@ExtendWith(MockitoExtension.class)
+public class JobMetadataExtractorServiceTest {
+    @Mock
+    private HtmlFetcherService fetcherService;
+
+    @Mock
+    private JobMetadataExtractor metadataExtractor;
+
+    @InjectMocks
+    private JobMetadataExtractorService jobMetadataExtractorService;
+
+    @BeforeEach
+    void setUp() {
+        /*HtmlFetcherService htmlFetcherService = mock(HtmlFetcherService.class);
+        JobMetadataExtractor jobMetadataExtractor = mock(JobMetadataExtractor.class);
+        jobMetadataExtractorService = new JobMetadataExtractorService(htmlFetcherService, List.of(jobMetadataExtractor));*/
+        jobMetadataExtractorService = new JobMetadataExtractorService(fetcherService, List.of(metadataExtractor));
+    }
+
+    @Test
+    public void testGetDomainFromInvalidUrl() {
+        assertThrows(MalformedUrlException.class, () -> jobMetadataExtractorService.getDomainFromUrl("not_an_url"));
+    }
+
+    @Test
+    public void testGetDomainFromIncompatibleUrl() {
+        assertThrows(MalformedUrlException.class, () -> jobMetadataExtractorService.getDomainFromUrl("ftp://my.ftp.example.com"));
+    }
+
+    @Test
+    public void testGetDomainFromUrlWithoutSubdomain() {
+        assertEquals("example.com", jobMetadataExtractorService.getDomainFromUrl("http://example.com"));
+    }
+
+    @Test
+    public void testGetDomainFromUrlWithOneSubdomain() {
+        assertEquals("example.com", jobMetadataExtractorService.getDomainFromUrl("https://www.example.com"));
+    }
+
+    @Test
+    public void testGetDomainFromUrlWithMultipleSubdomain() {
+        assertEquals("example.com", jobMetadataExtractorService.getDomainFromUrl("https://w3.www.fr.example.com"));
+    }
+
+    @Test
+    public void testEmptyHtmlExtraction() {
+        when(fetcherService.fetchHtml(anyString(), anyString())).thenReturn(Optional.empty());
+
+        JobMetadata expectedJobMetadata = new JobMetadata.Builder()
+                .url("http://example.com")
+                .build();
+
+        assertEquals(expectedJobMetadata, jobMetadataExtractorService.extractMetadata("http://example.com"));
+    }
+
+    @Test
+    public void testCompatibleHtmlExtraction() throws IOException {
+        when(fetcherService.fetchHtml(anyString(), anyString())).thenReturn(Optional.ofNullable(TestFileLoader.loadFileAsString("jobposting.jsonld.salary.monetaryamount.html")));
+        when(metadataExtractor.extractJobMetadata(anyString())).thenReturn(Optional.of(new JobMetadata.Builder()
+                .title("Formateur equipes commerciales h/f")
+                .description("<p>Quelles sont les missions ?</p><p>&nbsp;Rejoignez Le Service Formation !<br/>Votre r&ocirc;le&nbsp;<br/>Dans le cadre de notre d&eacute;veloppement, nous recherchons&nbsp;un Formateur Interne au niveau national&nbsp;d&eacute;di&eacute; Commerce,&nbsp;pour renforcer notre &eacute;quipe de formateurs. V&eacute;ritable ambassadeur de la performance commerciale et de l'excellence, vous jouerez un r&ocirc;le cl&eacute; dans le d&eacute;veloppement des comp&eacute;tences de nos &eacute;quipes commerciales.<br/>Vous accompagnerez nos collaborateurs commerciaux vers le succ&egrave;s en assurant leur formation + pilotage Sales Academy.<br/>Formation aux techniques de vente :<br/>\t-\tConcevoir, animer et &eacute;valuer des formations&nbsp;aux techniques de vente et sur nos produits et services<br/>\t-\tAssurer un suivi r&eacute;gulier des performances des collaborateurs form&eacute;s<br/>\t-\tAccompagnement terrain des commerciaux<br/>\t-\tIdentifier les besoins de formation et optimiser les processus d'apprentissage en collaboration avec avec la direction commerciale<br/></p><p>Quel est le profil idéal ?</p><p>Ing&eacute;nierie p&eacute;dagogique&nbsp;:<br/>\t-\tAdapter les contenus p&eacute;dagogiques en fonction des besoins des &eacute;quipes et des &eacute;volutions du march&eacute;.<br/>Veille et innovation :<br/>\t-\tProposer des outils et formats innovants pour favoriser l'apprentissage (e-learning, ateliers pratiques, &eacute;tudes de cas, etc.).<br/>\t-\tAssurer une veille sur les meilleures pratiques commerciales et les nouvelles m&eacute;thodes de formation<br/>Intervenant dans la Sales Academy et int&eacute;gration des nouveaux commerciaux :<br/>\t-\tStructurer et coordonner la Sales Academy, garantissant un parcours de formation coh&eacute;rent et efficace pour les nouveaux arrivants<br/>\t-\tOrganiser et superviser l'int&eacute;gration des nouveaux commerciaux pour assurer une mont&eacute;e en comp&eacute;tence rapide<br/>Ce poste est fait pour vous si ....<br/>Vous justifiez d'une exp&eacute;rience r&eacute;ussie dans le commerce dans la vente ou le management commercial et d'une exp&eacute;rience en formation<br/>\t-\tComp&eacute;tences p&eacute;dagogiques : capacit&eacute; &agrave; transmettre vos connaissances et &agrave; engager<br/>\t-\tOrganisation et leadership : piloter des projets, structurer des parcours de formation et mobiliser des &eacute;quipes.<br/>\t-\tD&eacute;placements &agrave; pr&eacute;voir dans toute la France 3j/semaine<br/>Pourquoi nous rejoindre ?<br/>\t-\tUn groupe International en croissance depuis plus de 10 ans<br/>\t-\tUn parcours d'int&eacute;gration et formation aux produits et services<br/>\t-\tUn management de proximit&eacute;, des &eacute;quipes dynamiques<br/>\t-\tOpportunit&eacute;s de d&eacute;veloppement professionnel et carri&egrave;re<br/>Avantages<br/>\t-\tMutuelle / Pr&eacute;voyance / CE / Compte Epargne Temps / RTT&nbsp;<br/>\t-\tVoiture de fonction&nbsp;<br/>R&eacute;mun&eacute;ration&nbsp;:&nbsp; &agrave; partir de 50K&curren; +10% de variable&nbsp;<br/>Statut cadre<br/>Visitez notre site :&nbsp;&nbsp;&nbsp;https://careers.rentokil-initial.com/fr/<br/>Rentosj<br/></p><p>Qui a publié cette offre ?</p><p>D&eacute;couvrez un univers professionnel &eacute;tonnant !<br/><br/>Depuis 1923, Initial Textile, acteur majeur de la blanchisserie industrielle, accompagne les soci&eacute;t&eacute;s qui souhaitent externaliser le nettoyage de leurs v&ecirc;tements et linge professionnels. <br/><br/>Plus de 50 000 clients de l'industrie, de l'h&ocirc;tellerie-restauration, artisans et commerces de proximit&eacute; nous font confiance.<br/><br/>Nos 25 sites en France g&egrave;rent la location/entretien du linge et la livraison avec l'ambition d'apporter le meilleur niveau de service.<br/><br/>Seul expert de ce march&eacute; &agrave; r&eacute;aliser la cr&eacute;ation de v&ecirc;tements, nos gammes allient confort, style et s&eacute;curit&eacute;, adapt&eacute;es aux sp&eacute;cificit&eacute;s de chaque m&eacute;tier. La tra&ccedil;abilit&eacute; de nos articles, apporte une valeur ajout&eacute;e unique &agrave; notre prestation de service.<br/><br/>Dans le cadre de notre politique RSE, 100% du linge est recycl&eacute; vers d'autres industries et nous poursuivons nos engagements sur la transition &eacute;nerg&eacute;tique.<br/><br/><br/><br/></p>")
+                .profile("Ingénierie pédagogique :\n\t-\tAdapter les contenus pédagogiques en fonction des besoins des équipes et des évolutions du marché.\nVeille et innovation :\n\t-\tProposer des outils et formats innovants pour favoriser l'apprentissage (e-learning, ateliers pratiques, études de cas, etc.).\n\t-\tAssurer une veille sur les meilleures pratiques commerciales et les nouvelles méthodes de formation\nIntervenant dans la Sales Academy et intégration des nouveaux commerciaux :\n\t-\tStructurer et coordonner la Sales Academy, garantissant un parcours de formation cohérent et efficace pour les nouveaux arrivants\n\t-\tOrganiser et superviser l'intégration des nouveaux commerciaux pour assurer une montée en compétence rapide\nCe poste est fait pour vous si ....\nVous justifiez d'une expérience réussie dans le commerce dans la vente ou le management commercial et d'une expérience en formation\n\t-\tCompétences pédagogiques : capacité à transmettre vos connaissances et à engager\n\t-\tOrganisation et leadership : piloter des projets, structurer des parcours de formation et mobiliser des équipes.\n\t-\tDéplacements à prévoir dans toute la France 3j/semaine\nPourquoi nous rejoindre ?\n\t-\tUn groupe International en croissance depuis plus de 10 ans\n\t-\tUn parcours d'intégration et formation aux produits et services\n\t-\tUn management de proximité, des équipes dynamiques\n\t-\tOpportunités de développement professionnel et carrière\nAvantages\n\t-\tMutuelle / Prévoyance / CE / Compte Epargne Temps / RTT \n\t-\tVoiture de fonction \nRémunération :  à partir de 50K? +10% de variable \nStatut cadre\nVisitez notre site :   https://careers.rentokil-initial.com/fr/\nRentosj\n")
+                .url(null)
+                .company("RENTOKIL INITIAL")
+                .salary("30000 - 60000 EUR / YEAR")
+                .build()))
+        ;
+
+        JobMetadata expectedJobMetadata = new JobMetadata.Builder()
+                .title("Formateur equipes commerciales h/f")
+                .description("<p>Quelles sont les missions ?</p><p>&nbsp;Rejoignez Le Service Formation !<br/>Votre r&ocirc;le&nbsp;<br/>Dans le cadre de notre d&eacute;veloppement, nous recherchons&nbsp;un Formateur Interne au niveau national&nbsp;d&eacute;di&eacute; Commerce,&nbsp;pour renforcer notre &eacute;quipe de formateurs. V&eacute;ritable ambassadeur de la performance commerciale et de l'excellence, vous jouerez un r&ocirc;le cl&eacute; dans le d&eacute;veloppement des comp&eacute;tences de nos &eacute;quipes commerciales.<br/>Vous accompagnerez nos collaborateurs commerciaux vers le succ&egrave;s en assurant leur formation + pilotage Sales Academy.<br/>Formation aux techniques de vente :<br/>\t-\tConcevoir, animer et &eacute;valuer des formations&nbsp;aux techniques de vente et sur nos produits et services<br/>\t-\tAssurer un suivi r&eacute;gulier des performances des collaborateurs form&eacute;s<br/>\t-\tAccompagnement terrain des commerciaux<br/>\t-\tIdentifier les besoins de formation et optimiser les processus d'apprentissage en collaboration avec avec la direction commerciale<br/></p><p>Quel est le profil idéal ?</p><p>Ing&eacute;nierie p&eacute;dagogique&nbsp;:<br/>\t-\tAdapter les contenus p&eacute;dagogiques en fonction des besoins des &eacute;quipes et des &eacute;volutions du march&eacute;.<br/>Veille et innovation :<br/>\t-\tProposer des outils et formats innovants pour favoriser l'apprentissage (e-learning, ateliers pratiques, &eacute;tudes de cas, etc.).<br/>\t-\tAssurer une veille sur les meilleures pratiques commerciales et les nouvelles m&eacute;thodes de formation<br/>Intervenant dans la Sales Academy et int&eacute;gration des nouveaux commerciaux :<br/>\t-\tStructurer et coordonner la Sales Academy, garantissant un parcours de formation coh&eacute;rent et efficace pour les nouveaux arrivants<br/>\t-\tOrganiser et superviser l'int&eacute;gration des nouveaux commerciaux pour assurer une mont&eacute;e en comp&eacute;tence rapide<br/>Ce poste est fait pour vous si ....<br/>Vous justifiez d'une exp&eacute;rience r&eacute;ussie dans le commerce dans la vente ou le management commercial et d'une exp&eacute;rience en formation<br/>\t-\tComp&eacute;tences p&eacute;dagogiques : capacit&eacute; &agrave; transmettre vos connaissances et &agrave; engager<br/>\t-\tOrganisation et leadership : piloter des projets, structurer des parcours de formation et mobiliser des &eacute;quipes.<br/>\t-\tD&eacute;placements &agrave; pr&eacute;voir dans toute la France 3j/semaine<br/>Pourquoi nous rejoindre ?<br/>\t-\tUn groupe International en croissance depuis plus de 10 ans<br/>\t-\tUn parcours d'int&eacute;gration et formation aux produits et services<br/>\t-\tUn management de proximit&eacute;, des &eacute;quipes dynamiques<br/>\t-\tOpportunit&eacute;s de d&eacute;veloppement professionnel et carri&egrave;re<br/>Avantages<br/>\t-\tMutuelle / Pr&eacute;voyance / CE / Compte Epargne Temps / RTT&nbsp;<br/>\t-\tVoiture de fonction&nbsp;<br/>R&eacute;mun&eacute;ration&nbsp;:&nbsp; &agrave; partir de 50K&curren; +10% de variable&nbsp;<br/>Statut cadre<br/>Visitez notre site :&nbsp;&nbsp;&nbsp;https://careers.rentokil-initial.com/fr/<br/>Rentosj<br/></p><p>Qui a publié cette offre ?</p><p>D&eacute;couvrez un univers professionnel &eacute;tonnant !<br/><br/>Depuis 1923, Initial Textile, acteur majeur de la blanchisserie industrielle, accompagne les soci&eacute;t&eacute;s qui souhaitent externaliser le nettoyage de leurs v&ecirc;tements et linge professionnels. <br/><br/>Plus de 50 000 clients de l'industrie, de l'h&ocirc;tellerie-restauration, artisans et commerces de proximit&eacute; nous font confiance.<br/><br/>Nos 25 sites en France g&egrave;rent la location/entretien du linge et la livraison avec l'ambition d'apporter le meilleur niveau de service.<br/><br/>Seul expert de ce march&eacute; &agrave; r&eacute;aliser la cr&eacute;ation de v&ecirc;tements, nos gammes allient confort, style et s&eacute;curit&eacute;, adapt&eacute;es aux sp&eacute;cificit&eacute;s de chaque m&eacute;tier. La tra&ccedil;abilit&eacute; de nos articles, apporte une valeur ajout&eacute;e unique &agrave; notre prestation de service.<br/><br/>Dans le cadre de notre politique RSE, 100% du linge est recycl&eacute; vers d'autres industries et nous poursuivons nos engagements sur la transition &eacute;nerg&eacute;tique.<br/><br/><br/><br/></p>")
+                .profile("Ingénierie pédagogique :\n\t-\tAdapter les contenus pédagogiques en fonction des besoins des équipes et des évolutions du marché.\nVeille et innovation :\n\t-\tProposer des outils et formats innovants pour favoriser l'apprentissage (e-learning, ateliers pratiques, études de cas, etc.).\n\t-\tAssurer une veille sur les meilleures pratiques commerciales et les nouvelles méthodes de formation\nIntervenant dans la Sales Academy et intégration des nouveaux commerciaux :\n\t-\tStructurer et coordonner la Sales Academy, garantissant un parcours de formation cohérent et efficace pour les nouveaux arrivants\n\t-\tOrganiser et superviser l'intégration des nouveaux commerciaux pour assurer une montée en compétence rapide\nCe poste est fait pour vous si ....\nVous justifiez d'une expérience réussie dans le commerce dans la vente ou le management commercial et d'une expérience en formation\n\t-\tCompétences pédagogiques : capacité à transmettre vos connaissances et à engager\n\t-\tOrganisation et leadership : piloter des projets, structurer des parcours de formation et mobiliser des équipes.\n\t-\tDéplacements à prévoir dans toute la France 3j/semaine\nPourquoi nous rejoindre ?\n\t-\tUn groupe International en croissance depuis plus de 10 ans\n\t-\tUn parcours d'intégration et formation aux produits et services\n\t-\tUn management de proximité, des équipes dynamiques\n\t-\tOpportunités de développement professionnel et carrière\nAvantages\n\t-\tMutuelle / Prévoyance / CE / Compte Epargne Temps / RTT \n\t-\tVoiture de fonction \nRémunération :  à partir de 50K? +10% de variable \nStatut cadre\nVisitez notre site :   https://careers.rentokil-initial.com/fr/\nRentosj\n")
+                .url("http://example.com")
+                .company("RENTOKIL INITIAL")
+                .salary("30000 - 60000 EUR / YEAR")
+                .build();
+
+        assertEquals(expectedJobMetadata, jobMetadataExtractorService.extractMetadata("http://example.com"));
+    }
+}
