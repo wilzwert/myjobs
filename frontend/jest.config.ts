a754f40f5779d97826f98e1b5883a1b7b6c7563e
Wilhelm Zwertvaegher <wilzwert@gmail.com>
2025-04-12 12:12:42 +0200

    chore(front): fix jest coverage target dir

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -4,4 +4,5 @@ import presets from 'jest-preset-angular/presets';
 export default {
   ...presets.createCjsPreset(),
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  coverageDirectory: './coverage/jest',
 } satisfies Config;
