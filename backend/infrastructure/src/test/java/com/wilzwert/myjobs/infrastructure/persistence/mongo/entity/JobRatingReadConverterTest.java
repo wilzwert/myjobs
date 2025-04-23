d14a7d6f135eeecf5e85e93b11cc4e8bcb6438bd
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2025-04-23 13:49:22 +0200

    test(infra): added some tests for JobRating converters

---
+++ backend/infrastructure/src/test/java/com/wilzwert/myjobs/infrastructure/persistence/mongo/entity/JobRatingReadConverterTest.java
@@ -0,0 +1,31 @@
+package com.wilzwert.myjobs.infrastructure.persistence.mongo.entity;
+
+import com.wilzwert.myjobs.core.domain.model.JobRating;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Wilhelm Zwertvaegher
+ * Date:23/04/2025
+ * Time:13:46
+ */
+
+public class JobRatingReadConverterTest {
+
+    private final JobRatingReadConverter converter = new JobRatingReadConverter();
+
+    @Test
+    void shouldConvertIntegerToJobRating() {
+        JobRating rating = converter.convert(4);
+        assertNotNull(rating);
+        assertEquals(4, rating.getValue());
+    }
+
+    @Test
+    void shouldThrowExceptionForInvalidValue() {
+        assertThrows(IllegalArgumentException.class, () -> converter.convert(6)); // supposition : 6 est invalide
+        assertThrows(IllegalArgumentException.class, () -> converter.convert(-1));
+        assertThrows(NullPointerException.class, () -> converter.convert(null));
+    }
+}
+
