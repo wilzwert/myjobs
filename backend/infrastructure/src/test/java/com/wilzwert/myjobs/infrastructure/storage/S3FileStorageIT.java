19ce67bffd54c35178eb5008ec06349c475eaa3a
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2025-04-23 15:24:58 +0200

    test(backend): setup backend env vars for backend CI

--- backend/infrastructure/src/test/java/com/wilzwert/myjobs/infrastructure/storage/S3FileStorageIT.java
+++ backend/infrastructure/src/test/java/com/wilzwert/myjobs/infrastructure/storage/S3FileStorageIT.java
@@ -2,7 +2,6 @@ package com.wilzwert.myjobs.infrastructure.storage;
 
 
 import com.wilzwert.myjobs.core.domain.ports.driven.FileStorage;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  * Time:09:44
  */
 @SpringBootTest
-// @EnabledIfSystemProperty(named = "spring.profiles.active", matches = "integration")
-@Disabled
+@EnabledIfSystemProperty(named = "spring.profiles.active", matches = "integration")
 public class S3FileStorageIT {
 
     @Autowired
