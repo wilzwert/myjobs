091244047dc35d15a3d35921cd584f575811d0c3
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2025-04-29 18:39:22 +0200

    test: improve Job unit tests

--- backend/core/src/test/java/com/wilzwert/myjobs/core/domain/model/job/JobTest.java
+++ backend/core/src/test/java/com/wilzwert/myjobs/core/domain/model/job/JobTest.java
@@ -20,9 +20,13 @@ public class JobTest {
     @Test
     public void shouldCreateJob() {
         UserId userId = new UserId(UUID.randomUUID());
+        JobId jobId = new JobId(UUID.randomUUID());
         Job job = Job.builder()
+                .id(jobId)
                 .url("http://www.example.com")
                 .title("Job title")
+                .status(JobStatus.CREATED)
+                .rating(JobRating.of(3))
                 .company("Job company")
                 .description("Job description")
                 .profile("Job profile")
@@ -33,6 +37,8 @@ public class JobTest {
         assertNotNull(job);
         assertEquals("Job title", job.getTitle());
         assertEquals("http://www.example.com", job.getUrl());
+        assertEquals(JobStatus.CREATED, job.getStatus());
+        assertEquals(JobRating.of(3), job.getRating());
         assertEquals("Job company", job.getCompany());
         assertEquals("Job description", job.getDescription());
         assertEquals("Job profile", job.getProfile());
@@ -43,9 +49,13 @@ public class JobTest {
     @Test
     public void shouldCreateJobFromOtherJob() {
         UserId userId = new UserId(UUID.randomUUID());
+        JobId jobId = new JobId(UUID.randomUUID());
         Job job = Job.builder()
+                .id(jobId)
                 .url("http://www.example.com")
                 .title("Job title")
+                .status(JobStatus.CREATED)
+                .rating(JobRating.of(3))
                 .company("Job company")
                 .description("Job description")
                 .profile("Job profile")
@@ -53,11 +63,15 @@ public class JobTest {
                 .userId(userId)
                 .build();
 
-        Job otherJob = Job.from(job).build();
+        JobId otherJobId = new JobId(UUID.randomUUID());
+        Job otherJob = Job.from(job).id(otherJobId).status(JobStatus.PENDING).build();
 
         assertNotNull(otherJob);
         assertEquals("Job title", otherJob.getTitle());
+        assertEquals(otherJobId, otherJob.getId());
         assertEquals("http://www.example.com", otherJob.getUrl());
+        assertEquals(JobStatus.PENDING, otherJob.getStatus());
+        assertEquals(JobRating.of(3), otherJob.getRating());
         assertEquals("Job company", otherJob.getCompany());
         assertEquals("Job description", otherJob.getDescription());
         assertEquals("Job profile", otherJob.getProfile());
