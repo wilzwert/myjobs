4d0e3f725a65f167e2f4e32c0549b041007635b2
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2025-04-29 14:16:23 +0200

    fix: use @NotNull instead of @NotBlank for CreateActivityRequest

--- backend/infrastructure/src/main/java/com/wilzwert/myjobs/infrastructure/api/rest/dto/CreateActivityRequest.java
+++ backend/infrastructure/src/main/java/com/wilzwert/myjobs/infrastructure/api/rest/dto/CreateActivityRequest.java
@@ -2,7 +2,7 @@ package com.wilzwert.myjobs.infrastructure.api.rest.dto;
 
 
 import com.wilzwert.myjobs.core.domain.model.activity.ActivityType;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 /**
@@ -16,7 +16,7 @@ import lombok.Data;
 public class CreateActivityRequest {
 
     // TODO @Schema(description = "")
-    @NotBlank(message = "FIELD_CANNOT_BE_EMPTY")
+    @NotNull(message = "FIELD_CANNOT_BE_EMPTY")
     private ActivityType type;
 
     private String comment;
