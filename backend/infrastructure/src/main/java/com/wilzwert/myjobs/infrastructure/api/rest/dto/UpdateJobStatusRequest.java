ecc38eb88e427da952dc699b64395331b198c8da
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2025-04-29 12:57:19 +0200

    fix: use @NotNull instead of @NotBlank to validate update status request

--- backend/infrastructure/src/main/java/com/wilzwert/myjobs/infrastructure/api/rest/dto/UpdateJobStatusRequest.java
+++ backend/infrastructure/src/main/java/com/wilzwert/myjobs/infrastructure/api/rest/dto/UpdateJobStatusRequest.java
@@ -2,7 +2,7 @@ package com.wilzwert.myjobs.infrastructure.api.rest.dto;
 
 
 import com.wilzwert.myjobs.core.domain.model.job.JobStatus;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 /**
@@ -15,6 +15,6 @@ import lombok.Data;
 // TODO @Schema(description = "Object expected for job status update request" )
 public class UpdateJobStatusRequest {
     // TODO @Schema(description = "")
-    @NotBlank(message = "FIELD_CANNOT_BE_EMPTY")
+    @NotNull(message = "FIELD_CANNOT_BE_EMPTY")
     private JobStatus status;
 }
