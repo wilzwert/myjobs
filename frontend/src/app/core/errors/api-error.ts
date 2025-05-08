0d4d51b6dee800a892e813d4a049817576650e1a
Wilhelm Zwertvaegher <wilzwert@gmail.com>
2025-05-08 15:09:59 +0200

    test(frontend): add tests for ApiError

--- frontend/src/app/core/errors/api-error.ts
+++ frontend/src/app/core/errors/api-error.ts
@@ -6,13 +6,10 @@ export class ApiError extends Error {
     errors: Record<string, string[]>;
     
     constructor(originalError: HttpErrorResponse) {
-        console.trace(originalError.status+'->'+originalError.message);
-        console.log(originalError);
-        console.log("errors?",originalError.error?.errors);
         const message: string = originalError.error?.message ?? 'Unable to load data';
         super(message);
         this.httpStatus = originalError.status;
         this.originalError = originalError;
-        this.errors = (originalError.error?.errors?? new Map<string, string[]>());
+        this.errors = (originalError.error?.errors?? {});
     }
 }
