98611c47a152a5f0b4967f6341ded4e828df3b1e
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2025-06-02 09:55:14 +0200

    feat(backend): make UserId serializable

--- backend/core/src/main/java/com/wilzwert/myjobs/core/domain/model/user/UserId.java
+++ backend/core/src/main/java/com/wilzwert/myjobs/core/domain/model/user/UserId.java
@@ -2,9 +2,10 @@ package com.wilzwert.myjobs.core.domain.model.user;
 
 import com.wilzwert.myjobs.core.domain.model.EntityId;
 
+import java.io.Serializable;
 import java.util.UUID;
 
-public record UserId(UUID value) implements EntityId<UUID> {
+public record UserId(UUID value) implements Serializable, EntityId<UUID> {
     public static UserId generate() {
         return new UserId(UUID.randomUUID());
     }
